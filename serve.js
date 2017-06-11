60c78b8418401a6ee1fb2a9905f482c140612521
claybeard <abharris@pugetsound.edu>
2017-06-11 13:33:55 -0700

    expand content with c&p

--- serve.js
+++ serve.js
@@ -3,8 +3,14 @@ var app = express()
 
 app.use(express.static('public'))
 
-app.get('/', function(req, res){
-  res.send('hello world')
+// app.get('/', function(req, res){
+//   res.send('hello world')
+// })
+
+app.set('view engine', 'jade')
+
+app.get('/resume', function(req, res){
+  res.sendfile('public/resume.html', {root: __dirname})
 })
 
 app.listen(3000, function(){
