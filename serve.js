5447cc389c8975793903e6962e996d13dac5fd57
claybeard <abharris@pugetsound.edu>
2017-06-13 19:38:06 -0700

    progress milestone one

--- serve.js
+++ serve.js
@@ -1,10 +1,11 @@
 var express = require('express')
 var app = express()
+var subdomain = require('express-subdomain')
 
 app.use(express.static('public'))
 
 // app.get('/', function(req, res){
-//   res.send('hello world')
+//   res.sendFile('public/index.html', {root: __dirname})
 // })
 
 app.set('view engine', 'jade')
@@ -16,3 +17,13 @@ app.get('/resume', function(req, res){
 app.listen(3000, function(){
   console.log('example app listening on port')
 })
+
+
+// var router = express.Router()
+//
+// router.get('/', function(req, res){
+//   res.send('Welcome to the api')
+// })
+//
+// app.use(subdomain('api', router))
+// app.listen(3000)
