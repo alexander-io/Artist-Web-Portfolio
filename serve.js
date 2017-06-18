d60a56b77761e51eb969a7742621b78c954a838c
claybeard <abharris@pugetsound.edu>
2017-06-18 03:26:19 -0700

    now the website has a database attached lolll

--- serve.js
+++ serve.js
@@ -6,27 +6,68 @@ var upload = multer()
 var subdomain = require('express-subdomain')
 
 
+
+var MongoClient = require('mongodb').MongoClient
+  , assert = require('assert');
+
+var url = 'mongodb://localhost:27017/portfolio'
+
+
+
+// here's a function that will insert a document
+var insertDocument = function(document, db, callback){
+  // get the documents collection
+  var collection = db.collection('messages')
+
+  try {
+    collection.insert(document)
+    console.log('successfully inserted document')
+    callback()
+  } catch (e) {
+    console.log('something went wrong inserting document :', e)
+  } finally {
+
+  }
+
+}
+
+
+
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended:true}))
 
 app.use(express.static('public'))
 
-// app.get('/', function(req, res){
-//   res.sendFile('public/index.html', {root: __dirname})
-// })
 
 app.set('view engine', 'jade')
-
-// app.get('/resume', function(req, res){
-//   res.sendfile('public/resume.html', {root: __dirname})
-// })
-
 app.post('/index.html', function(req, res){
-  // res.send('got post request')
-  // console.log('got post request :', req.params)
-  // console.log('req body:', req)
   console.log('req body : ', req.body)
   console.log('req ip :', req.ip)
+
+  let message_to_insert = {}
+
+  if (req.body.name && req.body.email){
+    console.log('user submited both name and email')
+    message_to_insert.name = req.body.name
+    message_to_insert.email = req.body.email
+    console.log('json message to insert : ', message_to_insert)
+
+    MongoClient.connect(url, function(err, db){
+      assert.equal(null, err)
+      console.log('connected to db server')
+      insertDocument(message_to_insert, db, function(){console.log('done inserting')})
+      db.close()
+    })
+
+  } else {
+    console.log('user only submitted name or email')
+  }
+  // req.body.name
+  // req.body.email
+  // req.body.message
+
+
+
   // res.end()
   // res.sendFile('public/index.html', {root: __dirname})
 
